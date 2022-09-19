c143b11f3ff4f326d7eb4b4574dfc25ec313aaf8
OPC-16 <chavan.p.omkar@gmail.com>
2022-09-19 18:11:02 +0530

    Create header file for ascii-art

---
+++ ascii_generator.h
@@ -0,0 +1,265 @@
+/* ---- Generate ascii art for choosen geometric figure ---- 
+	Author >_ OPC 
+*/
+
+void ascii_generator(int i) {
+	if(i == 1) {
+		//cube
+	printf("\t  +------+\n");
+	system("sleep 0.4");
+	printf("\t /      /|\n");
+	system("sleep 0.4");
+	printf("\t+------+ |\n");
+	system("sleep 0.4");
+	printf("\t|      | +\n");
+	system("sleep 0.4");
+	printf("\t|      |/\n");
+	system("sleep 0.4");
+	printf("\t+------+\n");
+	system("sleep 1");
+	}
+	else if(i == 2) {
+		//cuboid
+	printf("\t   o--------o\n");
+	system("sleep 0.4");
+	printf("\t  /|       /|\n");
+	system("sleep 0.4");
+	printf("\t / |      / |\n");
+	system("sleep 0.4");
+	printf("\to--------o  |\n");
+	system("sleep 0.4");
+	printf("\t|  o-----|--o\n");
+	system("sleep 0.4");
+	printf("\t| /      | /\n");
+	system("sleep 0.4");
+	printf("\t|/       |/\n");
+	system("sleep 0.4");
+	printf("\to--------o\n");
+	system("sleep 1");
+	}
+	else if(i == 3 || i == 8) {
+		//cylinder
+	printf("\t                   .------.\n");
+	system("sleep 0.4");
+	printf("\t                  (        )\n");
+	system("sleep 0.4");
+	printf("\t                  |~------~|\n");
+	system("sleep 0.4");
+	printf("\t                  |        |   .-----.\n");
+	system("sleep 0.4");
+	printf("\t                  |        |  |._____.'\n");
+	system("sleep 0.4");
+	printf("\t                  |        |  |       |\n");
+	system("sleep 0.4");
+	printf("\t                  |   .------.|       |\n");
+	system("sleep 0.4");
+	printf("\t                  |  (        |       |\n");
+	system("sleep 0.4");
+	printf("\t                  |  |~------~|       |\n");
+	system("sleep 0.4");
+	printf("\t                  |  |        |       |\n");
+	system("sleep 0.4");
+	printf("\t   _..----------..|  |  _.-----._     |\n");
+	system("sleep 0.4");
+	printf("\t.-~                ~-..-         -.   |\n");
+	system("sleep 0.4");
+	printf("\t|.                  .||-_       _-|   | \n");
+	system("sleep 0.4");
+	printf("\t|'-..____________..-'||  ~-----~  |   |\n");
+	system("sleep 0.4");
+	printf("\t|                   .`|           |--'\n");
+	system("sleep 0.4");
+	printf("\t '-..____________..-'' `._       _.'\n");
+	system("sleep 0.4");
+	printf("\t                         '-----''		\n");
+	system("sleep 1");	
+	}
+	else if(i == 4) {
+		//sphere
+	printf("\t         .d$#T!!!~'#*b.\n");
+	system("sleep 0.3");
+	printf("\t      d$MM!!!!~~~     'h\n");
+	system("sleep 0.3");
+	printf("\t    dRMMM!!!~           ^k\n");
+	system("sleep 0.3");
+	printf("\t   $RMM!!~                ?\n");
+	system("sleep 0.3");
+	printf("\t  $MMM!~                   '\n");
+	system("sleep 0.3");
+	printf("\t $MM!!                      9\n");
+	system("sleep 0.3");
+	printf("\t4RM!!                        F\n");
+	system("sleep 0.3");
+	printf("\t$!!!                         J\n");
+	system("sleep 0.3");
+	printf("\t9!!~                         F\n");
+	system("sleep 0.3");
+	printf("\t'k~~                        :\n");
+	system("sleep 0.3");
+	printf("\t 3>                         F\n");
+	system("sleep 0.3");
+	printf("\t  9>                       F\n");
+	system("sleep 0.3");
+	printf("\t   'i                    :'\n");
+	system("sleep 0.3");
+	printf("\t     t.                .P\n");
+	system("sleep 0.3");
+	printf("\t       #c.          .z#\n");
+	system("sleep 0.3");
+	printf("\t          ^#*heee*#'			\n");
+	system("sleep 1");
+	}
+	else if(i == 5) {
+		//cone
+	printf("\t⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀  ⣠⣄\n");
+	system("sleep 0.3");
+	printf("\t⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣿⣿⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n");
+	system("sleep 0.3");
+	printf("\t⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⣿⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n");
+	system("sleep 0.3");
+	printf("\t⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n");
+	system("sleep 0.3");
+	printf("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n");
+	system("sleep 0.3");
+	printf("\t⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣤⣄⣀⣀⣠⣤⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n");
+	system("sleep 0.3");
+	printf("\t⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n");
+	system("sleep 0.3");
+	printf("\t⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠻⠿⢿⣿⣿⡿⠿⠟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n");
+	system("sleep 0.3");
+	printf("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n");
+	system("sleep 0.3");
+	printf("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n");
+	system("sleep 0.3");
+	printf("\t⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⣿⣶⣤⣤⣤⣤⣤⣤⣶⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀\n");
+	system("sleep 0.3");
+	printf("\t⠀⠀⠀⠀⣀⣠⣤⡖⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⢶⣤⣄⣀⠀⠀⠀⠀\n");
+	system("sleep 0.3");
+	printf("\t⠀⠀⠀⠉⠙⠻⢿⣿⡀⠛⢿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠛⢀⣿⡿⠟⠋⠉⠀⠀⠀\n");
+	system("sleep 0.3");
+	printf("\t⠀⠀⠀⠀⠀⠀⠀⠀⠉⠁⠢⠤⣤⣀⣈⣁⣀⣤⠤⠔⠈⠉⠀⠀⠀⠀⠀⠀⠀⠀\n");
+	system("sleep 0.3");
+	printf("\t⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠛⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n");
+	system("sleep 1");
+	}
+	else if(i == 6) {
+		//tetrahedron
+	printf("\t ╔╗      ╔╗         ╔╗        ╔╗           \n");
+	system("sleep 0.4");
+	printf("\t╔╝╚╗    ╔╝╚╗        ║║        ║║           \n");
+	system("sleep 0.4");
+	printf("\t╚╗╔╝╔══╗╚╗╔╝╔═╗╔══╗ ║╚═╗╔══╗╔═╝║╔═╗╔══╗╔══╗ \n");
+	system("sleep 0.4");
+	printf("\t ║║ ║╔╗║ ║║ ║╔╝╚ ╗║ ║╔╗║║╔╗║║╔╗║║╔╝║╔╗║║╔╗╗\n");
+	system("sleep 0.4");
+	printf("\t ║╚╗║║═╣ ║╚╗║║ ║╚╝╚╗║║║║║║═╣║╚╝║║║ ║╚╝║║║║║\n");
+	system("sleep 0.4");
+	printf("\t ╚═╝╚══╝ ╚═╝╚╝ ╚═══╝╚╝╚╝╚══╝╚══╝╚╝ ╚══╝╚╝╚╝   \n");
+	system("sleep 1");
+	}
+	else if(i == 7) {
+		//pyramid
+	printf("\t                                           _L/L\n");
+	system("sleep 0.4");
+	printf("\t                                         _LT/l_L_\n");
+	system("sleep 0.4");
+	printf("\t                                       _LLl/L_T_lL_\n");
+	system("sleep 0.4");
+	printf("\t                   _T/L              _LT|L/_|__L_|_L_\n");
+	system("sleep 0.4");
+	printf("\t                 _Ll/l_L_          _TL|_T/_L_|__T__|_l_\n");
+	system("sleep 0.4");
+	printf("\t               _TLl/T_l|_L_      _LL|_Tl/_|__l___L__L_|L_\n");
+	system("sleep 0.4");
+	printf("\t             _LT_L/L_|_L_l_L_  _'|_|_|T/_L_l__T _ l__|__|L_\n");
+	system("sleep 0.4");
+	printf("\t           _Tl_L|/_|__|_|__T _LlT_|_Ll/_l_ _|__[ ]__|__|_l_L_\n");
+	system("sleep 0.4");
+	printf("\t    jjs_ _LT_l_l/|__|__l_T _T_L|_|_|l/___|__ | _l__|_ |__|_T_L_  __\n\n");
+	system("sleep 0.4");
+	printf("\t                           nn_r   nn_r                 __\n");
+	system("sleep 0.4");
+	printf("\t                     __   /l(\\   /l)\\      nn_r\n");
+	system("sleep 0.4");
+	printf("\t               __                         /\\(\\    __\n");
+	system("sleep 1");
+	}
+	else if(i == 9) {
+		//torus
+	printf("\t████████╗ ██████╗ ██████╗ ██╗   ██╗███████╗\n");
+	system("sleep 0.4");
+	printf("\t╚══██╔══╝██╔═══██╗██╔══██╗██║   ██║██╔════╝\n");
+	system("sleep 0.4");
+	printf("\t   ██║   ██║   ██║██████╔╝██║   ██║███████╗\n");
+	system("sleep 0.4");
+	printf("\t   ██║   ██║   ██║██╔══██╗██║   ██║╚════██║\n");
+	system("sleep 0.4");
+	printf("\t   ██║   ╚██████╔╝██║  ██║╚██████╔╝███████║\n");
+	system("sleep 0.4");
+	printf("\t   ╚═╝    ╚═════╝ ╚═╝  ╚═╝ ╚═════╝ ╚══════╝\n");
+	system("sleep 1");
+	}
+	else if(i == 10) {
+		//capsule
+	system("sleep 0.3");
+	printf("\t⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠻⣦⠈⠻⣷⣶⣄⡀\n");
+	system("sleep 0.3");
+	printf("\t⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⣦⡀⠠⣦⡈⠻⠋⣠⣦⠀\n");
+	system("sleep 0.3");
+	printf("\t⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⣿⣿⣿⣿⣦⡈⠻⣦⡈⠻⣿⣇\n");
+	system("sleep 0.3");
+	printf("\t⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⡈⠻⠂⠈⠻\n");
+	system("sleep 0.3");
+	printf("\t⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⡿⠋⠙⢿⡿⠋⠙⢿⣿⣿⣿⣿⣿⣦⡀⠻⣦\n");
+	system("sleep 0.3");
+	printf("\t⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⣏⠀⠀⠀⠀⠀⠀⠀⠀⣹⣿⣿⣿⣿⣿⠟⠀⠀\n");
+	system("sleep 0.3");
+	printf("\t⠀⠀⠀⠀⠀⢀⣴⣿⣿⣿⣿⡷⠀⠀⠀⠀⠀⠀⢾⣿⣿⣿⣿⠟⠁⠀\n");
+	system("sleep 0.3");
+	printf("\t⠀⠀⠀⠀⣴⣿⣿⣿⣿⣿⣏⠀⠀⠀⠀⠀⠀⠀⠀⣹⣿⠟⠁⠀\n");
+	system("sleep 0.3");
+	printf("\t⠀⠀⠻⣦⡈⠻⡿⢿⣿⣿⣿⣷⣄⣠⣾⣷⣄⣠⣾⠟⠁⠀\n");
+	system("sleep 0.3");
+	printf("\t⠀⠀⣦⡈⠻⠂⣠⣄⠙⢿⣿⣿⣿⣿⣿⣿⣿⠟⠁⠀\n");
+	system("sleep 0.3");
+	printf("\t⠀⠀⢹⣿⡀⠸⣿⣿⡷⠀⣹⣿⣿⣿⣿⠟⠁⠀\n");
+	system("sleep 0.3");
+	printf("\t⠀⠀⠀⠹⣿⣦⡈⠋⠠⣦⡈⠻⣿⠟⠁⠀\n");
+	system("sleep 0.3");
+	printf("\t⠀⠀⠀⠀⠈⠙⠿⢿⣦⡈⠻⣦⠀⠀\n");
+	system("sleep 1");
+	}
+	else if(i == 11) {
+		//ellipsoid
+	printf("\t       |  | _)                     _)      | \n");
+	system("sleep 0.4");
+	printf("\t  _ \\  |  |  |  __ \\    __|   _ \\   |   _` | \n");
+	system("sleep 0.4");
+	printf("\t  __/  |  |  |  |   | \\__ \\  (  |   |  (   | \n");
+	system("sleep 0.4");
+	printf("\t\\___| _| _| _|  .__/  ____/ \\___/  _| \\__._| \n");
+	system("sleep 0.4");
+	printf("\t               _|                            \n");
+	system("sleep 1");
+	}
+	else if(i == 333) {
+		//bye bye
+			printf("\n\n");
+			printf("\t   .    _  .     ___________________\n");
+			printf("\t   |\\_|/__/|    /                   \\\n");
+			printf("\t  / / \\/ \\  \\  / Thank You for using \\\n");
+			printf("\t /__|O||O|__ \\ \\   Volume Calculator /\n");
+			printf("\t|/_ \\_/\\_/ _\\ | \\  _________________/\n");
+			printf("\t| | (____) | ||  |/\n");
+			printf("\t\\/\\___/\\__/  // _/\n");
+			printf("\t(_/         ||\n");
+			printf("\t |          ||\\\n");
+			printf("\t  \\        //_/ \n");
+			printf("\t   \\______//\n");
+			printf("\t  __|| __||\n");
+			printf("\t (____(____)\n");
+			printf("");
+			system("sleep 1.5");
+	}
+
+} //ascii_generator
